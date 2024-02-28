2eabdf83451ac389bec4754e0843f1a8cf5138fc
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 13:17:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+//famous quote:Find a quote from a famous person you admire. Print the quote and the name of 
+//its author. Your output should look something like the following, including the quotation 
+//marks:
+//Albert Einstein once said, "Aperson who never made a mistake never tries anything new."
+var qoute = "A person who naver made a mistake never tried anything new";
+var author = "Albert Einstine";
+console.log("".concat(author, " once said, \"").concat(qoute, "\""));
---
+++ app.ts
@@ -0,0 +1,9 @@
+//famous quote:Find a quote from a famous person you admire. Print the quote and the name of 
+//its author. Your output should look something like the following, including the quotation 
+//marks:
+
+//Albert Einstein once said, "Aperson who never made a mistake never tries anything new."
+
+let qoute: string= "A person who naver made a mistake never tried anything new";
+let author: string= "Albert Einstine";
+console.log(`${author} once said, "${qoute}"`);
